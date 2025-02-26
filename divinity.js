a8e83835714d335757ad70962d9f1053ee626183
Sssammy-lab <tma854385@gmail.com>
2025-02-26 11:29:51 +0300

    Add files via upload

---
+++ divinity.js
@@ -0,0 +1,13 @@
+function fruitsandvegies(){
+    window.location.href="fruitsandvegies.html";
+}
+function dairy(){
+    window.location.href="dairy.html";
+}
+function bathroom_supplies(){
+    window.location.href="bathroomsupplies.html";
+}
+function cutlery(){
+    window.location.href="Kitchen_Accesories.html";
+}
+
